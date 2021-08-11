43f88f1c8fdc288e30819dcab2524eb3b400d8f2
mooncoderz <mooncoderz@gmail.com>
2021-08-11 11:07:16 +0500

    V2 - Added Indicators under slider

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const slides = document.querySelectorAll('.slides');
-let arrows = document.querySelectorAll('.arrow');
+const arrows = document.querySelectorAll('.arrow');
+const indicator = document.querySelectorAll('.indicator');
 
 flag = 0; // this is the index number of current slide
 
 
-// Show a particluar slide of a given parameter
+// Main Slider Function 
 let slidesShow = (index) =>{
     if(index == slides.length){
         flag = 0;
@@ -14,15 +15,31 @@ let slidesShow = (index) =>{
         index = slides.length -1;
     }else{
     }
+    // Hide Other Slides and change the color of inactive indicators
     slides.forEach(function(v,i){
         slides[i].style.display = "none";
+        indicator[i].style.background = "transparent";
+        indicator[i].style.border = "1px solid gray";
     });
+    // Show a particluar slide of a given parameter
     slides[index].style.display = "block";
+    indicator[index].style.background = 'gray';
 }
 slidesShow(flag);
 
-// Indicators Settings
-let indicaters = (num) =>{
+
+// Arrow Settings
+let arrow = (num) =>{
     flag = flag + num;
     slidesShow(flag);
+}
+
+// Indicators Settings
+let indicators = (ind) =>{
+    slidesShow(ind);
+    indicator.forEach(function(v,i){
+        indicator[i].style.border = "1px solid gray";
+        indicator[ind].style.background = 'transparent';
+    });
+    indicator[ind].style.background = 'gray';
 }
